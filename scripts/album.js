2844180ffadbd2921d480df2418e67de39af7c73
Isaac Kim <myong328@gmail.com>
2016-11-28 21:43:04 -0800

    assignment-21-completed

--- scripts/album.js
+++ scripts/album.js
@@ -42,7 +42,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         '<tr class="album-view-song-item">'
       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
-      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '  <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
       + '</tr>'
       ;
  
@@ -152,15 +152,48 @@ var createSongRow = function(songNumber, songName, songLength) {
      }
  };
 
+var setCurrentTimeInPlayerBar = function(currentTime){
+    var $currentTimeElement = $('.seek-control .current-time');
+    $currentTimeElement.text(currentTime);
+    
+};
+
+var setTotalTimeInPlayerBar = function(totalTime) { 
+     var $totalTimeElement = $('.seek-control .total-time');
+     $totalTimeElement.text(totalTime); 
+};
+
+var filterTimeCode = function(timeInSeconds){
+    var seconds = Number.parseFloat(timeInSeconds);
+     var wholeSeconds = Math.floor(seconds);
+     var minutes = Math.floor(wholeSeconds / 60);
+     
+     var remainingSeconds = wholeSeconds % 60;
+     var output = minutes + ':';
+     
+     if (remainingSeconds < 10) {
+         output += '0';   
+     }
+     
+     output += remainingSeconds;
+     return output;
+};
+    
+
+    
+
 var updateSeekBarWhileSongPlays = function() {
      if (currentSoundFile) {
          // #10
          currentSoundFile.bind('timeupdate', function(event) {
              // #11
-             var seekBarFillRatio = this.getTime() / this.getDuration();
+             var currentTime = this.getTime();
+             var songLength = this.getDuration();
+             var seekBarFillRatio = currentTime / songLength;
              var $seekBar = $('.seek-control .seek-bar');
  
              updateSeekPercentage($seekBar, seekBarFillRatio);
+             setCurrentTimeInPlayerBar(filterTimeCode(currentTime));
          });
      }
  };
@@ -217,7 +250,7 @@ var setupSeekBars = function() {
          });
  
          // #10
-         $(document).bind('mouseup.thumb', function() {
+        $(document).bind('mouseup.thumb', function() {
              $(document).unbind('mousemove.thumb');
              $(document).unbind('mouseup.thumb');
          });
